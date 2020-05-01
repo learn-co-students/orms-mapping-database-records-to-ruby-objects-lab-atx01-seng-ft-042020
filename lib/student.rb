4247dbc979a2d309091ad513ec48f992b226d7c0
marshall405 <marshall.slemp@gmail.com>
2020-04-30 23:11:55 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,78 @@
+require "pry"
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL 
+      SELECT * FROM students WHERE grade=9
+    SQL
+    DB[:conn].execute(sql).map do |student|
+      new_from_db(student)
+    end
+  end
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL 
+      SELECT * FROM students WHERE grade IS NOT "12"
+    SQL
+    DB[:conn].execute(sql).map do |student|
+      new_from_db(student)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(grade)
+    sql = <<-SQL 
+      SELECT * FROM students WHERE grade="10" LIMIT ?
+    SQL
+    DB[:conn].execute(sql, grade).map do |student|
+      new_from_db(student)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL 
+    SELECT * FROM students WHERE grade="10" LIMIT 1
+    SQL
+    new_from_db(DB[:conn].execute(sql).first)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL 
+    SELECT * FROM students WHERE grade=?
+    SQL
+    DB[:conn].execute(sql, grade).map do |student|
+      new_from_db(student)
+    end
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL 
+      SELECT * FROM students
+    SQL
+    students = DB[:conn].execute(sql)
+    students.map do |student|
+      new_from_db(student)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL 
+      SELECT * FROM students WHERE name=? LIMIT 1
+    SQL
+    new_from_db(DB[:conn].execute(sql, name).first)
   end
   
   def save
